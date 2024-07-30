ea6c136a7151f9576f58cfd4a40fcac2e85b6bc3
asmita <asmitajadhav049@gmail.com>
2024-07-30 11:52:41 +0530

    Initial commit

--- basic_if_else/Day.java
+++ basic_if_else/Day.java
@@ -1,7 +1,7 @@
 public class Day{
 	public static void main(String[]args){
 	 int arr[]=new int[] {2,4,6,8,1,2, 3,5};
-	 	
+	 	System.out.println("Asmita");
 	 for(int i=0;i<arr.length;i++){
 	   if(arr[i]%2!=0){
          System.out.print(arr[i]+ " ");
